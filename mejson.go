dff6c659057051a4647317f17f8e52c31e0e7954
Ian Hansen <ian@supershabam.com>
2014-02-06 14:54:40 -0700

    simpler handler for slices of things

--- mejson.go
+++ mejson.go
@@ -5,37 +5,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"labix.org/v2/mgo/bson"
+	"reflect"
 	"time"
 )
 
 func Marshal(in interface{}) ([]byte, error) {
-	switch v := in.(type) {
-	case []bson.M:
-		return MarshalMaps(v)
-	case bson.M:
-		return MarshalMap(v)
-	case bson.D:
-		// todo write marshaller for doc to ensure serialization order
-		return MarshalMap(v.Map())
-	case bson.Binary:
-		return MarshalBinary(v)
-	case bson.ObjectId:
-		return MarshalObjectId(v)
-	case time.Time:
-		return nil, fmt.Errorf("unimplemented type time.Time")
-	case string:
-		return json.Marshal(v)
-	default:
-		fmt.Printf("unknown type: %T\n", v)
-		return json.Marshal(v)
+	if reflect.TypeOf(in).Kind() == reflect.Slice {
+		v := reflect.ValueOf(in)
+		slice := []interface{}{}
+		for i := 0; i < v.Len(); i++ {
+			slice = append(slice, v.Index(i).Interface())
+		}
+		return MarshalSlice(slice)
+	} else {
+		switch v := in.(type) {
+		case bson.M:
+			return MarshalMap(v)
+		case bson.D:
+			// todo write marshaller for doc to ensure serialization order
+			return MarshalMap(v.Map())
+		case bson.Binary:
+			return MarshalBinary(v)
+		case bson.ObjectId:
+			return MarshalObjectId(v)
+		case time.Time:
+			return nil, fmt.Errorf("unimplemented type time.Time")
+		case string:
+			return json.Marshal(v)
+		default:
+			fmt.Printf("unknown type: %T\n", v)
+			return json.Marshal(v)
+		}
 	}
 }
 
-func MarshalObjectId(in bson.ObjectId) ([]byte, error) {
-	return json.Marshal(map[string]string{"$oid": in.Hex()})
-}
-
-func MarshalMaps(in []bson.M) ([]byte, error) {
+func MarshalSlice(in []interface{}) ([]byte, error) {
 	result := []*json.RawMessage{}
 	for _, value := range in {
 		bytes, err := Marshal(value)
@@ -47,6 +51,10 @@ func MarshalMaps(in []bson.M) ([]byte, error) {
 		result = append(result, message)
 	}
 	return json.Marshal(result)
+}
+
+func MarshalObjectId(in bson.ObjectId) ([]byte, error) {
+	return json.Marshal(map[string]string{"$oid": in.Hex()})
 }
 
 func MarshalMap(in bson.M) ([]byte, error) {
