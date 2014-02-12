76f12eb4b025c6444956d71562232a1affd08fc3
Ian Hansen <ian@supershabam.com>
2014-02-12 11:30:18 -0700

    adds documentation

--- bsonify.go
+++ bsonify.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+// Bsonify turns a naive json map[string]interface{} object into a bson.M object.
+// It will turn mongodb extended json fields (like $oid) into an appropriate golang object.
+// e.g. {"_id":{"$oid":"52ddcd077854f173de9429b3"}} will become {"_id":bson.ObjectId("52ddcd077854f173de9429b3")}
+// Find the documentation on MongoDB Extended JSON here: http://docs.mongodb.org/manual/reference/mongodb-extended-json/
 func Bsonify(in map[string]interface{}) (result bson.M, err error) {
 	if false {
 		fmt.Println("meow")
